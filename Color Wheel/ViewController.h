60893f64e443ea1bdd2c6787d02b6a68d92c0431
grackend <grackend@github.com>
2014-06-20 14:53:53 -0700

    Initial Commit

---
+++ Color Wheel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Color Wheel
+//
+//  Created by Grace Kendall on 6/20/14.
+//  Copyright (c) 2014 Grace Kendall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Color Wheel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Color Wheel
+//
+//  Created by Grace Kendall on 6/20/14.
+//  Copyright (c) 2014 Grace Kendall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
